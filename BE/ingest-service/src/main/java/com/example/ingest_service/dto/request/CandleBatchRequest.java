f91b1a839e34d5e0b6dadcc83af600e17ecc7617
vuden2605 <vuden2605@gmail.com>
2026-01-22 13:15:45 +0700

    fix

--- BE/ingest-service/src/main/java/com/example/ingest_service/dto/request/CandleBatchRequest.java
+++
@@ -1,9 +0,0 @@
-package com.example.ingest_service.dto.request;
-
-import java.util.List;
-
-public class CandleBatchRequest {
-	public String symbol;
-	public String interval;
-	public List<Candle> candles;
-}
